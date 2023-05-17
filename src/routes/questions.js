32107c3b05b254e7e854897c22d101107abd7f78
Caleb tenHaaf <calebtenHaaf08@gmail.com>
2023-05-17 13:50:09 +0000

    added questons

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -11,7 +11,8 @@ export const questions = [
        question: "When should you reach out for help?",
        answers: [
            "As soon as possible",
-           "Only the day before class"
+           "Only the day before class",
+           "Day of class"
        ],
        correct: 0,
        time: 20
@@ -36,5 +37,40 @@ export const questions = [
        ],
        correct: 2,
        time: 20
-   }
-]
+   },
+   {
+        question: " What are the Dutch wooden shoes called?",
+        answers: [
+            "Wood shoes",
+            "Clogs",
+            "Dutch slip-ons",
+        ],
+        correct: 1,
+        time: 20
+   },
+   {
+    question: " What base number system is composed of just zeros and ones?",
+    answers: [
+        "Base-0",
+        "Base-1",
+        "Base-10",
+        "Base-2",
+    ],
+    correct: 3,  
+    time: 20
+   },
+   {
+    question: "In 1952 what computer predicted the outcome of the U.S election?",
+    answers: [
+        "Altair 8800",
+        "Univac",
+        "Altair 8000",
+        "Pilot ACE",
+    ],
+    correct: 1,
+    time: 20
+   },
+   
+
+  ]
+
